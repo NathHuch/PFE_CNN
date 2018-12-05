cd70fe40418ae0d84fd09f2ea6c05d58727f0603
NathHuch <nathan.huchon@gmail.com>
2018-12-05 16:18:26 +0100

    Nouvelle création de matrices Son, elles sont normalisées entre -1 et 1 pour les feed au RNA

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-########################################################################################################################
-#################################################   Librairies   #######################################################
-########################################################################################################################
 import os, os.path
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,10 +17,92 @@ number_files = len(list)
 index = 0
 while index!=number_files:
     os.chdir("C:\\Users\\Nathan\\Documents\\Nathan_Travail\\I3\\PFE\\Python\\Creation_Spectre")
+
     # On récupère les fichiers audio générés et les métadonnées stockées dans le fichier json
-
-
     [fe, data] = read(dir+'\\'+list[index])
+    nb_sources = 1#data.shape[1]
+    data_length = data.shape[0]
+    data = data/np.maximum(np.abs(max(data)),np.abs(min(data)))
+
+    # Sous- échantillonnage pour arriver à une fréquence d'échantillonnage de fe/4 = 11025 Hz
+    data = data[0:data_length:4]
+    fse = fe / 4
+    data_length = data.shape[0]
+
+    # Paramètres des STFT
+    NFFT = 2**9 # taille des fft
+
+    f, t, Zxx = stft(data, fse, nperseg=NFFT-1, nfft=NFFT-1)
+    result = np.empty([np.shape(Zxx)[0], np.shape(Zxx)[1]], dtype='complex_')
+    result[:, :] = Zxx
+
+    # Calcul des Densité spectrales de puissance
+    PSD      = np.zeros((int(NFFT/2), result.shape[1]))
+    PSD_MFCC = np.zeros((int(NFFT/2), result.shape[1]))
+    Energie  = np.zeros((result.shape[1]))
+    Label    = np.zeros((result.shape[1]))
+    for compo in np.arange(0, result.shape[1]):
+        PSD[:, compo] = 20*np.log10(np.abs(result[:, compo] * np.conjugate(result[:, compo])))
+        PSD_MFCC[:, compo] = 1125 * np.log((1 + (np.abs(result[:, compo] * np.conjugate(result[:, compo])))/700))
+
+        ###classement de données poubelles
+        # Calcul de l'énergie d'un spectrograme pour des fréquences corresspondantes à la voix humaine
+        # ie intégrale sous la courbe du spectrogramme courrant.
+        Energie[compo] = np.sum(PSD_MFCC[:, compo])
+        #Classement en fonction de l'énergie (voix ou silence)
+        if Energie[compo]>1e-5:
+            Label[compo] = 1
+        else:
+            Label[compo] = 0
+
+    # On va ensuite classer nos imagettes entre voix ou bruit
+    # filtrage passe bas-de la taille de nos imagettes
+    Y = np.correlate(Label,np.ones((12)))
+
+    for rang in np.arange(0,len(Y)):
+        # Au cas ou on a un mélange parfait de bruit et de voix
+        if Y[rang]==0:
+            vect  = np.arange(rang,rang+24)
+            PSD_MFCC = np.delete(PSD_MFCC,vect,axis=1)
+            PSD_MFCC = np.delete(PSD_NORM,vect,axis=1)
+
+    # Normalisation des données pour le RNA une fois que les max sont détectés
+    PSD_NORM = 2*((PSD-PSD.min())/(PSD.max()-PSD.min()))-1
+    PSD_MFCC = 2 *((PSD_MFCC - PSD_MFCC.min()) / (PSD_MFCC.max() - PSD_MFCC.min())) - 1
+
+    # Sauvegarde des spectres dans les dossiers suivants
+    os.chdir("C:\\Users\\Nathan\\Documents\\Nathan_Travail\\I3\\PFE\\Python\\Creation_Spectre\\Matrice_images\\MFC")
+    np.savetxt(list[index][:-3]+'txt', PSD_MFCC)
+    os.chdir("C:\\Users\\Nathan\\Documents\\Nathan_Travail\\I3\\PFE\\Python\\Creation_Spectre\\Matrice_images\\20LOG10")
+    np.savetxt(list[index][:-3]+'txt', PSD_NORM)
+    index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+    #np.loadtxt(list[index][:-3]+'txt')
+
+    # object_pi = math.pi
+    # file_pi = open('filename_pi.obj', 'w')
+    # pickle.dump(object_pi, file_pi)
+    # # Récupération des données
+    # x_lignes = int(np.ceil(np.shape(signal)[0] / fft_NFFT)) * fft_NFFT
+    # x = np.empty([x_lignes, np.shape(data)[1] + 1])
+    #
+    # for channel in np.arange(0, data_nb_channel):
+    #     t, x[:, channel] = istft(result[:, :, channel], data_fe, nperseg=fft_NFFT)
+    #     t, x[:, channel + 1] = istft(result[:, :, channel + 1], data_fe, nperseg=fft_NFFT)
+    #
+    # retrieve_signals = x[0:np.shape(data)[0], :]
+    # time = t[0:np.shape(data)[0]]
     ########################################################################################################################
     ############################################   Sélection des sources ##################################################
     ########################################################################################################################
@@ -32,12 +111,8 @@ while index!=number_files:
     #     d = json.load(json_data)
     #     print(d)
 
-    # # On récupère théoriquement le nb de microphones présents dans la scène grâce au nombre de voies
-    nb_sources = 1#data.shape[1]
-    data_length = data.shape[0]
-    data = data/np.maximum(np.abs(max(data)),np.abs(min(data)))
     # Visualisation des signaux d'entrées
-    t = np.arange(0, data_length / fe, 1 / fe)
+    #t = np.arange(0, data_length / fe, 1 / fe)
 
     # plt.figure()
     # plt.plot(t,data)
@@ -45,12 +120,6 @@ while index!=number_files:
     # plt.xlabel("Temps en (s)")
     # plt.ylabel("Amplitude in (V)")
     # plt.show()
-
-    # Sous- échantillonnage pour arriver à une fréquence d'échantillonnage de fe/4 = 11025 Hz
-    data = data[0:data_length:4]
-    # mise à jour des nouveaux paramètres
-    fse = fe / 4
-    data_length = data.shape[0]
 
     # Nouvelle visuaisation possible des données
     # tse = np.arange(0,data_length/fse,1/fse)
@@ -63,9 +132,6 @@ while index!=number_files:
 
     # Libération de la mémoire avant d'effectuer les calculs principaux
     # del t,tse
-
-    # Paramètres des STFT
-    NFFT = 2**10 # taille des fft
 
     # retard_estimé = estimation_delay(donéées,taille_des_fenêtres_étudiées,voie à comparer)
     # retard = estimation_delay(data, NFFT, 0)
@@ -86,34 +152,6 @@ while index!=number_files:
     # f   : Vecteur fréquentiel permettant d'afficher les STFT
     # t   : Vecteur temporel permettant d'afficher les STFT
     # Zxx : Matrice contenant les STFT
-    f, t, Zxx = stft(data, fse, nperseg=NFFT-1, nfft=NFFT-1)
-    result = np.empty([np.shape(Zxx)[0], np.shape(Zxx)[1]], dtype='complex_')
-    result[:, :] = Zxx
-
     # On applique ce calcul sur les autres sources
     # for channel in np.arange(1, nb_sources):
     #     f, t, result[:, :, channel] = stft(signal[:, channel], fse, nperseg=NFFT-1, nfft=NFFT-1)
-
-    ## Calcul des Densité spectrales de puissance
-    PSD = np.zeros((int(NFFT/2), result.shape[1]))
-    for compo in np.arange(0, result.shape[1]):
-        # for channel in np.arange(0, nb_sources):
-            PSD[:, compo] = 20*np.log10(np.abs(result[:, compo] * np.conjugate(result[:, compo])))
-    dir = "Donnes_wav"
-    os.chdir("C:\\Users\\Nathan\\Documents\\Nathan_Travail\\I3\\PFE\\Python\\Creation_Spectre\\Matrice_images")
-    np.savetxt(list[index][:-3]+'txt', PSD)
-    #np.loadtxt(list[index][:-3]+'txt')
-    index +=1
-    # object_pi = math.pi
-    # file_pi = open('filename_pi.obj', 'w')
-    # pickle.dump(object_pi, file_pi)
-    # # Récupération des données
-    # x_lignes = int(np.ceil(np.shape(signal)[0] / fft_NFFT)) * fft_NFFT
-    # x = np.empty([x_lignes, np.shape(data)[1] + 1])
-    #
-    # for channel in np.arange(0, data_nb_channel):
-    #     t, x[:, channel] = istft(result[:, :, channel], data_fe, nperseg=fft_NFFT)
-    #     t, x[:, channel + 1] = istft(result[:, :, channel + 1], data_fe, nperseg=fft_NFFT)
-    #
-    # retrieve_signals = x[0:np.shape(data)[0], :]
-    # time = t[0:np.shape(data)[0]]
